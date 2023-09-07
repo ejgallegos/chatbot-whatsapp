bae62224a1f46344677616aaf0d738fde947ae0e
Eric Gallegos <ejgallegos@gmail.com>
2023-09-07 14:58:29 -0300

    add icons

--- helpers/constantsResponse.js
+++ helpers/constantsResponse.js
@@ -1,27 +1,28 @@
 const MSJ_OPCIONES = {
-    "elegir-opcion": "Elige el número correspondiente a una de las opciones que te presenté.",
-    "opcion-invalida": "Ingresá una opción válida.",
-    "opcion-cantidad-noches": "Ingresá la cantidad de noches.",
+    "elegir-opcion": "👆 Elige el número correspondiente a una de las opciones que te presenté.",
+    "opcion-invalida": "❌ Ingresá una opción válida.",
+    "opcion-cantidad-noches": "❌ Ingresá la cantidad de noches.",
 };
 
 const MSJ_FECHAS = {
-    "fecha-formato-incorrecto": "Ingresá una fecha con el formato correcto.",
-    "fecha-posterior": "Ingresá una fecha posterior a la actual.",
-    "fecha-incorrecta": "Ingresá una fecha correcta.",
-    "fecha-no-disponible": "Lamentablemente no tenemos disponibilidad para la fecha que ingresaste.",
+    "fecha-formato-incorrecto": "❌ Ingresá una fecha con el formato correcto.",
+    "fecha-posterior": "❌ Ingresá una fecha posterior a la actual.",
+    "fecha-incorrecta": "❌ Ingresá una fecha correcta.",
+    "fecha-no-disponible": "🤦🏻‍♂️ Lamentablemente no tenemos disponibilidad para la fecha que ingresaste.",
 };
 
 const MSJ_CONFIRMACION = {
-    "confirmacion-previa": "En pocos minutos se contactará un Agente para confirmar tu Reserva.",
-    "confirmacion-ok": "¡Gracias por la confirmación!",
+    "confirmacion-previa": "En pocos minutos se contactará un 👨🏻‍💼 Agente para confirmar tu Reserva.",
+    "confirmacion-ok": "✅ ¡Gracias por la confirmación!",
 };
 
 const MSJ_CIERRE_FLUJO = {
-    "cierre-despedida": "¡Gracias por la comunicación, estoy atento para una nueva conversación!",
+    "cierre-despedida": "👋 ¡Adiós! Gracias por la comunicación, estoy atento para una nueva conversación.",
 };
 
 const MSJ_ERROR = {
-    "error-servicio": "Disculpa por el inconveniente.",
+    "error-servicio": "🙏 Disculpa por el inconveniente.",
+    "error-intenta-nuevamente": "🙏 Te pido disculpas, ¿Puedes intentar nuevamente en unos minutos? Gracias.",
 };
 
 module.exports = { MSJ_OPCIONES, MSJ_FECHAS, MSJ_CONFIRMACION, MSJ_CIERRE_FLUJO, MSJ_ERROR };
