7f4815424e50232b1ee753b1b6acfd2b16bc5e4a
Eric Gallegos <ejgallegos@gmail.com>
2023-09-07 14:58:17 -0300

    add menu

--- helpers/constantsMenu.js
+++ helpers/constantsMenu.js
@@ -1,6 +1,6 @@
 const MENU = {
     "menu-principal": "*1)* Reserva fecha\n*2)* Buscar fecha disponible\n*3)* Ver Alojamientos\n*4)* Salir",
+    "menu-cant-personas": "*1)* Una\n*2)* Dos\n*3)* Tres\n*4)* Cuatro\n*5)* Cinco\n*6)* Seis",
 };
-
 
 module.exports = { MENU };
